6b5420a426deaa49dbe8bc91bd36ad680f0c8578
Binoy C Ranjith <90437954+BinoyCRanjith@users.noreply.github.com>
2022-10-03 19:08:05 +0530

    Update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,3 +9,20 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule // CLI adds AppRoutingModule to the AppModule's imports array
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
