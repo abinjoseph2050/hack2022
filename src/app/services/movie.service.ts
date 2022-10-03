9a898d3bcab55f93317fc473934257457cf5448c
Binoy C Ranjith <90437954+BinoyCRanjith@users.noreply.github.com>
2022-10-03 19:12:11 +0530

    updatedmovies.service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -27,3 +27,24 @@ export class MovieService {
     }
 }
 }
+export class EmployeeClass {
+  private id: number;
+  private firstName: string;
+  private lastName: string;
+  private salary: number;
+
+  constructor(id: number, firstName: string, lastName, salary: number) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.salary = salary;
+  }
+
+  getName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getYearlySalary(): number {
+    return 12 * this.salary;
+  }
+}
